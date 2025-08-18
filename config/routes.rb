eb98e09af1b08b827d590a58757bfc1c33d0d4ec
Rafael Neves <rafaelneves0405@gmail.com>
2025-08-17 23:14:07 -0300

    fix: remove routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :sales
-  resources :posts
+  resources :posts, only: [:index]
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
